38fbffc700fd62776e26c3ef669f3ec52ac6c9fa
yoshihiro.yoshida <marchwind0079@gmail.com>
2020-12-09 18:55:57 +0900

    fix: add create docs for runServer

--- src/main/scala/sample/http/Server.scala
+++ src/main/scala/sample/http/Server.scala
@@ -5,20 +5,26 @@ import org.http4s.HttpApp
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import sample.{AppEnv, UserServiceEnv}
-import sample.http.routes.HealthCheckRoutes
+import sample.http.routes.{HealthCheckRoutes, UserRoutes}
 import sample.services.healthcheck.HealthCheck
 import sample.system.config.Config
+import sttp.tapir.openapi.circe.yaml.RichOpenAPI
+import sttp.tapir.swagger.http4s.SwaggerHttp4s
 import zio.clock.Clock
 import zio.interop.catz._
 import zio.interop.catz.implicits._
-import zio.{blocking, Runtime, Task, URIO, ZIO}
+import zio.{Runtime, Task, URIO, ZIO, blocking}
 
 object Server {
 
+  private val userDocs = UserRoutes.docs.toYaml
+
   private val appRoutes: URIO[UserServiceEnv with HealthCheck, HttpApp[Task]] =
     for {
+      userRoutes        <- UserRoutes.routes
       healthCheckRoutes <- HealthCheckRoutes.routes
-    } yield (healthCheckRoutes).orNotFound
+      docsRoutes         = new SwaggerHttp4s(userDocs).routes[Task]
+    } yield (userRoutes <+> healthCheckRoutes <+> docsRoutes).orNotFound
 
   val runServer: ZIO[AppEnv, Throwable, Unit] =
     for {
@@ -26,14 +32,15 @@ object Server {
       svConfig                     <- Config.httpServerConfig
       implicit0(r: Runtime[Clock]) <- ZIO.runtime[Clock]
       bec                          <- blocking.blocking(ZIO.descriptor.map(_.executor.asEC))
-      _ <- BlazeServerBuilder[Task](bec)
-        .bindHttp(svConfig.port.value, svConfig.host.value)
-        .withoutBanner
-        .withNio2(true)
-        .withHttpApp(app)
-        .serve
-        .compile
-        .drain
+      _ <-
+        BlazeServerBuilder[Task](bec)
+          .bindHttp(svConfig.port.value, svConfig.host.value)
+          .withoutBanner
+          .withNio2(true)
+          .withHttpApp(app)
+          .serve
+          .compile
+          .drain
     } yield ()
 
 }
